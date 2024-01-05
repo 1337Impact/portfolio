3725f94ee1df5ed1a333a2134840bd1fbe915c25
Mohammed Benkhattab <mobenkhattab@gmail.com>
2024-01-05 10:45:35 +0100

    testing resend email sending

--- src/app/api/sendmail/route.ts
+++ src/app/api/sendmail/route.ts
@@ -1,27 +1,20 @@
 import schema from "@/utils/zod";
+import { Resend } from "resend";
 
-export const runtime = "edge";
-export const dynamic = "force-dynamic";
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: Request) {
   try {
     const req = await request.json();
     const body = schema.parse(req);
     console.log(body);
-    const res = await fetch("https://api.resend.com/emails", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.RESEND_API_KEY}`,  
-      },
-      body: JSON.stringify({
-        from: "benkhattab.me <onboarding@resend.dev>",
-        to: "mobenkhattab@gmail.com",
-        subject: `${body.subject}`,
-        text: `Sent by: "${body.name}" Email: "${body.email}"\n\n${body.message}`,
-      }),
+    const { data, error } = await resend.emails.send({
+      from: "benkhattab.me <onboarding@resend.dev>",
+      to: "mobenkhattab@gmail.com",
+      subject: `${body.subject}`,
+      text: `Sent by: "${body.name}" Email: "${body.email}"\n\n${body.message}`,
     });
-    if (!res.ok) {
+    if (error) {
       throw new Error("Bad Request");
     }
     return Response.json({ message: "success" });
